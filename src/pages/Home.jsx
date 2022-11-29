c9ce2a004160066798bc98e1235266e6497e910a
LeoDavila1994 <liodavila.ld@gmail.com>
2022-11-29 17:44:41 -0600

    slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import isotipo from "../Imgs/logos/logo trans.png"
+import sliderImg from "../Imgs/Movile/r-architecture-2gDwlIim3Uw-unsplash.jpg";
 
 const Home = () => {
   return (
     <div className='home-container'>
       <article className='home-slider'>
         <img src={isotipo} className='isotipo'></img>
-        <div className='home-slider-img'></div>
+        <div className='home-slider-img' style={{backgroundImage: `linear-gradient(45deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${sliderImg})`}}></div>
       </article>
       <div className='home-message'>
         <p>TU CASA TU MEJOR INVERSION</p>
