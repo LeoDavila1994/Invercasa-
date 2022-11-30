5c60d3e5eab2e29e8ac438245280831999f059b8
LeoDavila1994 <liodavila.ld@gmail.com>
2022-11-29 18:55:03 -0600

    home change

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Options = ({ img, letter }) => {
 
   return (
-    <div className='option-container' style={{backgroundImage: `linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img})`}}>
+    <div className='option-container' style={{backgroundImage: `url(${img})`}}>
       <p>{letter}</p>
     </div>
   );
